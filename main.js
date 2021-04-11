d64e439c0a6af3d6c304cf23f9d1c81f3157fb24
Sakina1209 <73835193+Sakina1209@users.noreply.github.com>
2021-04-11 14:12:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 prediction1 = "";
-prediction2 = "";
 
 Webcam.set({
     width : 300,
@@ -12,7 +11,7 @@ camera = document.getElementById("camera");
 
 Webcam.attach(camera);
 
-function take_snap(){
+function snap(){
     Webcam.snap(function(data_uri){
  document.getElementById("result").innerHTML = '<img id = "captured_img" src= "'+ data_uri+ '"/>';
     });
@@ -23,4 +22,38 @@ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
 
 function modelLoaded(){
     console.log("MODEL LOADED !!!");
+}
+
+function speakcomp(){
+    var synth = window.speechSynthesis;
+    speak_data1 = "The first prediction is" + prediction1;
+    var utterthis = new SpeechSynthesisUtterance(speak_data1);
+    synth.speak(utterthis);
+}
+
+function check(){
+    img = document.getElementById('captured_img');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error){
+        console.error(error);
+    } else {
+        console.log(results);
+        prediction1 = results[0].label;
+        document.getElementById("result_emotion_name").innerHTML = prediction1;
+
+        speakcomp()
+
+        if (prediction1 == "Best"){
+            document.getElementById("result_emoji_name").innerHTML = "👍";
+        }
+        if (prediction1 == "Amazing"){
+            document.getElementById("result_emoji_name").innerHTML = "👌";
+        }
+        if (prediction1 == "Peace"){
+            document.getElementById("result_emoji_name").innerHTML = "✌";
+        }
+    }
 }
